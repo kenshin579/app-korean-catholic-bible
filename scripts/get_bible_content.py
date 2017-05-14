fc5457927b454e00741109bf0707407805bca00f
Administrator <user@example.com>
2017-05-14 15:42:20 +0900

    bible sample for test

--- scripts/get_bible_content.py
+++ scripts/get_bible_content.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+################################################
+# TODO LIST
+# 1. 가톨릭 웹사이트에서 성경 가져와서 ....
+# - 구약 성경: http://info.catholic.or.kr/bible/list.asp?gubun=old2
+# - 신약: http://info.catholic.or.kr/bible/list.asp?gubun=new2
+################################################
